33122addf707a3c273b58dac64a5ed4aa2ea9444
sakura <hiperperu85@gmail.com>
2026-04-17 15:06:34 -0500

    route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,1 +1,14 @@
+export const runtime = 'edge';
 
+export async function GET(request: Request) {
+  return new Response(
+    JSON.stringify({
+      mensaje: "¡Hola! Mi primera Edge Function",
+      fecha: new Date().toISOString()
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    }
+  );
+}
